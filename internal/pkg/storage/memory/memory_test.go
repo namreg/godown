478612d6277929894b61a3b70e000cbc79ce8ce8
Igor German <iggerman@yandex.ru>
2018-06-08 15:45:07 +0300

    Write tests for memory package

---
+++ internal/pkg/storage/memory/memory_test.go
@@ -0,0 +1,218 @@
+package memory
+
+import (
+	"errors"
+	"reflect"
+	"testing"
+	"time"
+
+	"github.com/namreg/godown-v2/internal/pkg/storage"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNew(t *testing.T) {
+	strg := New()
+	assert.Equal(t, map[storage.Key]*storage.Value{}, strg.items)
+	assert.Equal(t, map[storage.Key]*storage.Value{}, strg.itemsWithTTL)
+}
+
+func TestStorage_Put(t *testing.T) {
+	type args struct {
+		key    storage.Key
+		setter storage.ValueSetter
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		{
+			name: "setter_returns_nil_value_and_nil_err",
+			args: args{
+				key:    storage.Key("key1"),
+				setter: func(*storage.Value) (*storage.Value, error) { return nil, nil },
+			},
+			wantErr: false,
+		},
+		{
+			name: "setter_returns_nil_value_and_err",
+			args: args{
+				key:    storage.Key("key1"),
+				setter: func(*storage.Value) (*storage.Value, error) { return nil, errors.New("error") },
+			},
+			wantErr: true,
+		},
+		{
+			name: "setter_returns_value_and_nil_err",
+			args: args{
+				key:    storage.Key("key1"),
+				setter: func(*storage.Value) (*storage.Value, error) { return storage.NewStringValue("test"), nil },
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			strg := &Storage{
+				items:        make(map[storage.Key]*storage.Value),
+				itemsWithTTL: make(map[storage.Key]*storage.Value),
+			}
+			if err := strg.Put(tt.args.key, tt.args.setter); (err != nil) != tt.wantErr {
+				t.Errorf("Storage.Put() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
+func TestStorage_Put_WhenValueShouldBeDeleted(t *testing.T) {
+	strg := &Storage{
+		items:        make(map[storage.Key]*storage.Value),
+		itemsWithTTL: make(map[storage.Key]*storage.Value),
+	}
+	strg.items[storage.Key("key1")] = storage.NewListValue("value1")
+	strg.itemsWithTTL[storage.Key("key1")] = storage.NewListValue("value1")
+
+	assert.NoError(t, strg.Put(storage.Key("key2"), func(*storage.Value) (*storage.Value, error) {
+		return nil, nil
+	}))
+
+	_, ok := strg.items[storage.Key("key2")]
+	assert.False(t, ok)
+
+	_, ok = strg.itemsWithTTL[storage.Key("key2")]
+	assert.False(t, ok)
+}
+
+func TestStorage_Put_WhenValueShouldBeAdded(t *testing.T) {
+	strg := &Storage{
+		items:        make(map[storage.Key]*storage.Value),
+		itemsWithTTL: make(map[storage.Key]*storage.Value),
+	}
+
+	assert.NoError(t, strg.Put(storage.Key("key"), func(*storage.Value) (*storage.Value, error) {
+		val := storage.NewStringValue("value")
+		val.SetTTL(time.Now().Add(1 * time.Second))
+		return val, nil
+	}))
+
+	_, ok := strg.items[storage.Key("key")]
+	assert.True(t, ok)
+
+	_, ok = strg.itemsWithTTL[storage.Key("key")]
+	assert.True(t, ok)
+}
+
+func TestStorage_Get(t *testing.T) {
+	strg := Storage{
+		items: map[storage.Key]*storage.Value{storage.Key("key"): storage.NewStringValue("value")},
+	}
+	tests := []struct {
+		name string
+		key  storage.Key
+		want *storage.Value
+		err  error
+	}{
+		{
+			name: "existing_key",
+			key:  storage.Key("key"),
+			want: storage.NewStringValue("value"),
+		},
+
+		{
+			name: "not_existing_key",
+			key:  storage.Key("not_existing_key"),
+			want: nil,
+			err:  storage.ErrKeyNotExists,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := strg.Get(tt.key)
+			if err != nil && err != tt.err {
+				t.Errorf("Storage.Get() error = %v, want err %v", err, tt.err)
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Storage.Get() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestStorage_Del(t *testing.T) {
+	strg := Storage{
+		items: map[storage.Key]*storage.Value{
+			storage.Key("key"):  storage.NewStringValue("value"),
+			storage.Key("key2"): storage.NewStringValue("value2"),
+		},
+		itemsWithTTL: map[storage.Key]*storage.Value{
+			storage.Key("key"): storage.NewStringValue("value"),
+		},
+	}
+
+	assert.NoError(t, strg.Del(storage.Key("key")))
+
+	assert.Equal(
+		t,
+		map[storage.Key]*storage.Value{
+			storage.Key("key2"): storage.NewStringValue("value2"),
+		},
+		strg.items,
+	)
+
+	assert.Equal(
+		t,
+		map[storage.Key]*storage.Value{},
+		strg.itemsWithTTL,
+	)
+}
+
+func TestStorage_Keys(t *testing.T) {
+	strg := Storage{
+		items: map[storage.Key]*storage.Value{
+			storage.Key("key"):  storage.NewStringValue("value"),
+			storage.Key("key2"): storage.NewStringValue("value2"),
+		},
+	}
+	expected := []storage.Key{storage.Key("key"), storage.Key("key2")}
+
+	actual, err := strg.Keys()
+
+	assert.NoError(t, err)
+	assert.Equal(t, expected, actual)
+}
+
+func TestStorage_All(t *testing.T) {
+	strg := Storage{
+		items: map[storage.Key]*storage.Value{
+			storage.Key("key"):  storage.NewStringValue("value"),
+			storage.Key("key2"): storage.NewStringValue("value2"),
+		},
+	}
+	expected := map[storage.Key]*storage.Value{
+		storage.Key("key"):  storage.NewStringValue("value"),
+		storage.Key("key2"): storage.NewStringValue("value2"),
+	}
+	actual, err := strg.All()
+
+	assert.NoError(t, err)
+	assert.Equal(t, expected, actual)
+}
+
+func TestStorage_AllWithTTL(t *testing.T) {
+	strg := Storage{
+		items: map[storage.Key]*storage.Value{
+			storage.Key("key"):  storage.NewStringValue("value"),
+			storage.Key("key2"): storage.NewStringValue("value2"),
+		},
+		itemsWithTTL: map[storage.Key]*storage.Value{
+			storage.Key("key2"): storage.NewStringValue("value2"),
+		},
+	}
+	expected := map[storage.Key]*storage.Value{
+		storage.Key("key2"): storage.NewStringValue("value2"),
+	}
+	actual, err := strg.AllWithTTL()
+
+	assert.NoError(t, err)
+	assert.Equal(t, expected, actual)
+}
